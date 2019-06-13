2972dd5aa82511ce1b8ea143d20894d6d5caa414
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-06-13 13:20:12 -0400

    -tidied up the codebase with additional comments - updated paper tables - ensured replicability by fixing a seed value for randomness

--- scripts/01_basic_model.R
+++ scripts/01_basic_model.R
@@ -1,65 +1,85 @@
+# Ensure we are starting with a clean-slate
 rm(list=ls())
+
+# Set the seed for any future randomization needs
+set.seed(0)
+
+# Load used libraries
 library(readr)
 library(tidyverse)
 library(broom)
 
-dataset = read_csv("./data/2011_qx_data.csv", col_types = cols(
-  Year = col_integer(),
-  Age = col_character(),
-  Country = col_factor(),
-  StatName = col_factor(),
-  .default = col_double()
-))
 
-# Constants
+# Read in the merged dataset for 2011
+dataset = read_csv("./data/01_processed/2011_qx_data.csv", 
+   col_types = cols(
+      Year = col_integer(),
+      Age = col_integer(), # note that this includes a value of "110+", this value is not used at the moment
+      Country = col_character(),
+      StatName = col_factor(),
+      .default = col_double()
+  ))
+  
+
+# Set Constants
 lambda_multiplier = 10^5
 epsilon = 1E-10
-x = 35:95
 
-dataset %>% 
+#################################################
+# Stage One - Estimating Sub-Group Parameters
+#################################################
+dataset %>% rename(qx = Value, x = Age, Gender = StatName) %>% 
   # limit the data to be between the ages of 35 to 95
-  filter( Age %in% paste0(x) ) %>% 
-  group_by(Country, StatName) %>% nest %>%  
+  filter( between(x, 35, 95) ) %>% 
   # Run the following on each country/gender combination
-  mutate(
+  group_by(Country, Gender) %>% nest %>% mutate(
     # Create a sequence of lambdas between 0 and the minimum lambda 
-    #  note that Value == qx
-    lambdas = map(data, ~ seq(0, min(.$Value), 1/lambda_multiplier) ),
+    lambdas = map(data, ~ seq(0, min(.$qx), 1/lambda_multiplier) ),
     # fit a linear model against each lambda
     # then keep the model-lambda combination with the best fit 
     model = map2(data, lambdas, function(data, lambdas) {
-      qx = data$Value
-      lapply(lambdas, function(lambda) {
-        y = log(log(1/(1-qx))-lambda+epsilon)
-        list(model=lm(y~x), lambda=lambda)
-      }) %>% reduce(function(l, r) {
+      lapply(lambdas, function(lambda) {list(
+          model  = lm(y~x, data=data_frame(
+            y = log(log(1/(1-data$qx))-lambda+epsilon),
+            x = data$x
+          )), 
+          lambda = lambda
+      )}) %>% reduce(function(l, r) {
         if( sigma(l$model) < sigma(r$model) ) l
         else r
       })
     })
   ) %>%
-  # compute parameters
+  # Compute Parameters
   mutate(
     lambda_makeham = map_dbl(model, ~.$lambda)*lambda_multiplier,
     K0 = map_dbl(model, ~.$model$coefficients[1]),
     g = map_dbl(model, ~.$model$coefficients[2]),
     lnh = K0 - log(exp(g-1)/g)
   ) -> 
+  Stage01_results
+
+#################################################
+# Stage One - View and Save Results
+#################################################
+# Take a look at  Table - 1
+Stage01_results %>% ungroup() %>% 
+  left_join(read_csv("./data/01_processed/country_codes.csv")) %>% 
+  arrange(Country, Gender) %>% 
+  select(`Country Name`, Gender, lambda_makeham, K0, g, lnh) ->
   Table1
 
-# Take a look at  Table - 1
-Table1 %>% ungroup() %>% 
-  left_join(read_csv("./data/01_processed/country_codes.csv")) %>% 
-  arrange(Country, StatName) %>% 
-  select(`Country Name`, StatName, lambda_makeham, K0, g, lnh) %>% 
-  View("Table - 1 -")
+Table1 %>% View("Table - 1 -")
+Table1 %>% write_csv("data/02_paper_tables/Table-01.csv")
 
-# Construct Table 2
-Table1 %>% ungroup() %>%
+#################################################
+# Stage Two - CLaM
+#################################################
+Stage01_results %>% ungroup() %>%
   # Drop the columns with model and lambdas
   select(-c(model, data, lambdas)) %>% 
   # Group by gender
-  group_by(StatName) %>% nest %>% 
+  group_by(Gender) %>% nest %>% 
   mutate(
     # Run the second regression
     model = map(data, ~lm(lnh~g, data=.)),
@@ -74,9 +94,15 @@ Table1 %>% ungroup() %>%
       N = nrow(.x)
     ))
   ) %>% unnest(data) ->
+  Stage2_data
+
+#################################################
+# Stage Two - View and Save Results
+#################################################
+Stage2_data %>% ungroup() %>% 
+  select(-model) ->
   Table2
 
-# Take a look at Table 2
-Table2 %>% ungroup() %>% 
-  select(-model) %>% 
-  View("Table - 2 -")
+Table2 %>% View("Table - 2 -")
+Table2 %>% write_csv("data/02_paper_tables/Table-02.csv")
+  
