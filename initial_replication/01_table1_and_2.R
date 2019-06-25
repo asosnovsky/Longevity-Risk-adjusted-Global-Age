3b5cfc5708582ba1f538e0f272d9b96480507426
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-06-24 20:25:38 -0400

    removed iceland

--- initial_replication/01_table1_and_2.R
+++ initial_replication/01_table1_and_2.R
@@ -1,9 +1,9 @@
 rm(list=ls())
 
 library(readr)
-library(tidyverse)
 library(readxl)
-library(scales)
+library(tidyverse)
+source("./scripts/00_method.R")
 
 # Read Data
 cc = read_csv("./initial_replication/manual_coef.csv") %>%  
@@ -20,38 +20,17 @@ World_Mortality_2011 %>%
 ##########################
 #   Table 1 Generation
 ##########################
-# filter to between 35 and 95 (replicating table 1)
-narrow_dt %>% filter(between(Age, 35, 95)) %>% 
+narrow_dt %>% 
+  # Remove Iceland
+  filter(Country != "ISL") %>% 
+  # filter to between 35 and 95 (replicating table 1)
+  filter(between(Age, 35, 95)) %>% 
   # Join the data-set with manually entered coefficients
   inner_join(cc, by=c("Country", "Gender")) %>% 
-  # create lambda values and 'y'
-  mutate( 
-    l = log(1/(1-qx)),
-    y = log(l-(lambda_makeham)*10^-5)  
-  ) %>% 
-  # Segment the dataset by country and gender
-  group_by(`Country Name`, Gender) %>% nest %>% 
-  mutate(
-    # Fit model
-    model = map(data, ~lm(y~Age ,data=.)),
-    # Extracts params
-    l_m = map_dbl(data, ~unique(.$lambda_makeham)),
-    params = map(model, ~data_frame(
-      K0 = .$coefficients[1],
-      g = .$coefficients[2],
-      lnh = as.numeric(K0-log((exp(g)-1)/g)),
-      m = (log(g) - lnh)/g,
-      b = 1/g
-    ))
-  ) %>% 
-  select(-c(data, model)) %>% 
-  unnest(params) -> stage1_model
+  compute_stage1 -> stage1_model
 
 # Format table numbers (make the numbers more presentatble)
-stage1_model %>% 
-  mutate( g = g %>% percent ) %>% 
-  mutate_if(is.numeric, ~round(., 3)) %>% arrange(Gender) %>% 
-  select(Gender, `Country Name`, lnh, l_m, g, m, b) -> table1
+stage1_model %>% compute_table1 -> table1
 
 # Save Table1a
 table1 %>% filter(Gender == "M") %>% select(-Gender) %>% 
@@ -71,39 +50,10 @@ stage1_model %>% group_by(Gender) %>%
 ##########################
 #   Table 2 Generation
 ##########################
-stage1_model %>% group_by(Gender) %>% nest %>% 
-  mutate(
-    # Run the lin-reg
-    model = map(data, ~summary(lm( lnh~g, data=. )) ),
-    # Extract computed params
-    L = map_dbl(model, ~.$coefficients[1,1]),
-    `x*` = -map_dbl(model, ~.$coefficients[2,1]),
-    G = map_dbl(data, ~mean(.$g))
-  ) -> stage2_model
+stage1_model %>% compute_stage2 -> stage2_model
 
 # Format numbers
-stage2_model %>% mutate(params = map(model, ~data_frame(
-  `6_Adj. R^2` = .$adj.r.squared %>% percent,
-  `2_L Std.Err` = .$coefficients[1,2],
-  `3_-x* Std.Err` = .$coefficients[2,2],
-  `4_L t-value` = .$coefficients[1,3],
-  `5_-x* t-value` = .$coefficients[2,3],
-))) %>% unnest(params) %>% 
-  mutate(params = map(data, ~data_frame(
-    `7_Range: g` = paste0("(", percent(min(.$g)), ', ', percent(max(.$g)), ')' ),
-    `9_Countries` = nrow(.)
-  ))) %>% unnest(params) %>% 
-  mutate_if(is.numeric, ~round(., 2)) %>% 
-  rename(
-    `0_L` = L,
-    `1_x*` = `x*`,
-    `8_G` = G
-  ) %>% 
-  select(-data, -model) %>% 
-  gather(stat, value, -Gender) %>% 
-  spread(Gender, value) %>% 
-  separate(stat, c("idx", "Statistic"), sep='_') %>% 
-  select(-idx) -> table2
+stage2_model %>% compute_table2 -> table2
 
 # Save to csv
 table2 %>% write_csv("./initial_replication/01_table2.csv")
--- scripts/01_models.R
+++ scripts/01_models.R
@@ -12,56 +12,66 @@ library(scales)
 
 # Read in the merged dataset for 2011
 dataset = read_csv("./data/01_processed/2011_qx_data.csv", 
-   col_types = cols(
-       Year = col_integer(),
-       Age = col_integer(), # note that this includes a value of "110+", this value is not used at the moment
-       Country = col_character(),
-       StatName = col_character(),
-       .default = col_double()
-     )
+                   col_types = cols(
+                     Year = col_integer(),
+                     Age = col_integer(), # note that this includes a value of "110+", this value is not used at the moment
+                     Country = col_character(),
+                     StatName = col_character(),
+                     .default = col_double()
+                   )
 )
+
+World_Mortality_2011 <- read_excel("data/World_Mortality_2011.xlsx", skip = 1)
+
+World_Mortality_2011 %>% 
+  gather(Country_Sex, Value, -c(Year, Age)) %>% 
+  separate(Country_Sex, c("Country", "StatName"), '_') -> dataset
 
 
 # Set Constants
 lambda_multiplier = 10^5
-epsilon = 1E-10
+epsilon = 1E-15
 
 #################################################
 # Stage One - Estimating Sub-Group Parameters
 #################################################
 dataset %>% rename(Gender=StatName, qx=Value) %>%
+  filter(Country == "AUS") %>% 
   # Limit to Just Ages between 35 to 95
   filter(between(Age, 35, 95)) %>% 
   # Generate initial lambda values
   mutate( l = log(1/(1-qx)) ) %>%
   # Group by Country and Gender, so that we may run the operations on each of the sub-groups
   group_by(Country, Gender) %>% nest() %>% 
-  mutate(
-    model = map(data, function(data) lapply(
-      # Create a sequence of potential accidental death lambdas
-      seq(from=0, to=min(data$l), by=1/lambda_multiplier), 
-      function(l_m) list(
-        # Fit a linear-regression model for each acc-death lambda per sub-group
-        model = lm(y~x, data=data_frame(
-          y = log(data$l-l_m+epsilon),
-          x = data$Age
-        )), 
-        l_m = l_m
-        # Only keep the best model per sub-group
-      )) %>% reduce(function(l, r) {
-        if( sigma(l$model) < sigma(r$model) ) l
-        else r
-      })
-    )
-  ) %>% 
+  mutate( model = map(data, ~lapply(
+    # Create a sequence of potential accidental death lambdas
+    seq(from=1, to=min(.$qx)*lambda_multiplier-1, by=1), 
+    function(l_m) list(
+      # Fit a linear-regression model for each acc-death lambda per sub-group
+      model = summary(lm(y~x, data=data_frame(
+        y = log(.$l-l_m/lambda_multiplier+epsilon),
+        x = .$Age
+      ))), 
+      l_m = l_m
+      # Only keep the best model per sub-group
+    )) %>% reduce(function(l, r) {
+      if( l$model$sigma < r$model$sigma ) l
+      else r
+    })
+  ) ) %>% 
   # Extract Model Parameters
   mutate(
-    K0 = map_dbl(model, ~.$model$coefficients[1]),
-    K1 = map_dbl(model, ~.$model$coefficients[2]),
+    K0 = map_dbl(model, ~.$model$coefficients[1,1]),
+    K1 = map_dbl(model, ~.$model$coefficients[2,1]),
     l_m = map_dbl(model, ~.$l_m),
+    l_min = map_dbl(data, ~min(.$qx))*lambda_multiplier,
+    #l_max = map_dbl(data, ~max(.$l))*lambda_multiplier,
     g = K1,
-    lnh = K0 - log( exp(K1-1)/K1 )
-  ) -> Stage1_model
+    lnh = K0 - log( exp(K1-1)/K1 ),
+    m = (log(g) - lnh)/g,
+    b = 1/g
+  ) %>% select(-data, -model)
+-> Stage1_model
 
 Stage1_model %>% write_rds("./data/02_models/stage1.rds")
 
