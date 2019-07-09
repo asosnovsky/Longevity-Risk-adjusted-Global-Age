098d60a1150a929e435470a5eab67718c490f806
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-07-09 10:03:20 -0400

    added titles

--- reports/Revised Tables/plots.R
+++ reports/Revised Tables/plots.R
@@ -10,7 +10,9 @@ Stage1_model %>% inner_join(country_codes, by="Country Name") %>%
   rename(`lnh[i]`=lnh, `g[i]`=g) %>% 
   ggplot(aes(`g[i]`, `lnh[i]`)) + geom_point(color='red') +
   geom_smooth(size=1/4, method='lm', se=F, col='black') +
-  geom_text(aes(label=`CountryCode`), size=2, nudge_x=-.0007)
+  geom_text(aes(label=`CountryCode`), size=2, nudge_x=-.0007) +
+  ggtitle("Female") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 ggsave(paste0(dir_save, "fig1_female.png"))
 
@@ -19,7 +21,10 @@ Stage1_model %>% inner_join(country_codes, by="Country Name") %>%
   rename(`lnh[i]`=lnh, `g[i]`=g) %>% 
   ggplot(aes(`g[i]`, `lnh[i]`)) + geom_point(color='blue') +
   geom_smooth(size=1/4, method='lm', se=F, col='black') +
-  geom_text(aes(label=`CountryCode`), size=2, nudge_x=-.0007)
+  geom_text(aes(label=`CountryCode`), size=2, nudge_x=-.0007) +
+  ggtitle("Male") +
+  theme(plot.title = element_text(hjust = 0.5))
+
 
 ggsave(paste0(dir_save, "fig1_male.png"))
 
@@ -34,8 +39,10 @@ Stage3_model %>% filter(Age == 55) %>%
     xmin = B_Age_Lower,
     xmax = B_Age_Upper,
     x = B_Age
-  ), fill='#0a0a0a') +
-  geom_vline(xintercept = 55, linetype="dotted")
+  ), size=0.15, fill='#0a0a0a') +
+  geom_vline(xintercept = 55, linetype="dotted") +
+  ggtitle("FEMALE: Chronological Age 55") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 ggsave(paste0(dir_save, "fig2_female.png"))
 
@@ -50,8 +57,10 @@ Stage3_model %>% filter(Age == 55) %>%
     xmin = B_Age_Lower,
     xmax = B_Age_Upper,
     x = B_Age
-  ), fill='#0a0a0a') +
-  geom_vline(xintercept = 55, linetype="dotted") 
-  ggsave("test.png")
+  ), size=0.15 , fill='#0a0a0a') +
+  geom_vline(xintercept = 55, linetype="dotted")+
+  ggtitle("MALE: Chronological Age 55") +
+  theme(plot.title = element_text(hjust = 0.5))
 
-  ggsave(paste0(dir_save, "fig2_male.png"))
+
+ggsave(paste0(dir_save, "fig2_male.png"))
